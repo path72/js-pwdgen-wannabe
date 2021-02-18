551e363a41406e9e0733af1f3004434491180d0d
Salvatore Trimarchi <path72@gmail.com>
2021-02-18 16:01:52 +0100

    end

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 //###################################################### 
 //
 
+var usrName, 
+    usrSurName, 
+    usrColor, 
+    usrSuffix, 
+    pwdReturn;
+
+usrName     = prompt('Scrivi il tuo nome:');
+usrSurName  = prompt('Scrivi il tuo cognome:');
+usrColor    = prompt('Qual è il tuo colore preferito?');
+usrSuffix   = 19;
+pwdReturn   = usrName + usrSurName + usrColor + usrSuffix;
+
+document.getElementById('pwd_return').innerHTML = pwdReturn;
 
 //###################################################### 
 //
+
 
